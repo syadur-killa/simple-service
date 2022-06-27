c0215160e09fdd9389056a17b82d93db4f34e6f9
syadur-killa <syadur.killa@gmail.com>
2022-06-27 11:40:46 +0600

    Footer add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Sheared/Header/Header';
+import Footer from './Sheared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }

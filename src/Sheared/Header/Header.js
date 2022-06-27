83df1836950686d89fbaa62099a1ec101aa39582
syadur-killa <syadur.killa@gmail.com>
2022-06-27 11:37:34 +0600

    Header add

---
+++ src/Sheared/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <ul style={{margin: "10px"}}>
+                <a style={{margin: "10px"}} href="">Test</a>
+                <a style={{margin: "10px"}} href="">Home</a>
+                <a style={{margin: "10px"}} href="">About</a>
+                <a style={{margin: "10px"}} href="">Services</a>
+                
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
